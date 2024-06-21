49f539107627889c4500cec1bfc53227535638f1
Francesco Spinella <francescospinella76@gmail.com>
2024-06-21 17:36:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ EXIT = 5
 
 
 #I CREATE A DICTIONARY: VALUE == NAME BOOK, KEY == NUMBER OF COPIES AVIABLE
-books = {
+library = {
     "Clean Code": 5,
     "Padre ricco Padre povero": 10,
     "Linguaggio C": 3,
@@ -26,42 +26,44 @@ def menu():
     print()
 
 #FUNCTION TO ADD A BOOK
-def add_book(dictionary):
+def add_book(library):
+    try:
+        book = input("Enter a name's book: ") # INPUT REQUEST FOR KEY == NAME BOOK, AND VALUE == NUMBER OF COPIES
+        copies = int(input("Enter a number of copies: "))
 
-    book = input("Enter a name's book: ") # INPUT REQUEST FOR KEY == NAME BOOK, AND VALUE == NUMBER OF COPIES
-    copies = int(input("Enter a number of copies: "))
+        library[book] = copies # ADDING KEY-VALUE TO DICTIONARY 
 
-    dictionary[book] = copies # ADDING KEY-VALUE TO DICTIONARY 
-
-    print(f"{book} has been added to library\n")  # PRINT OF UPADTE DICTONARY 
+        print(f"'{book}' has been added to library\n")  # PRINT OF UPADTE DICTONARY
+    except ValueError:
+        print("Error! Enter the number of copies in the copies field\n") 
 
 #FUNCTION TO VIEW AVIABLE BOOK
-def view_books(dictonary):
-    if len(dictonary) == 0:
+def view_books(library):
+    if len(library) == 0:
         print("No books present in the library.\n") # IF LENGHT OF DICT IS 0, PRINT NO BOOK PRESENT IN THE LIBRARY
     else:
-        print("Book\tNumber of copies")
-        for book, copies in dictonary.items():
-            print(f"{book} : {copies}")
+        print("List of available books:")
+        for book, copies in library.items():
+            print(f"{book} : {copies} copies")
         print()
         
 #FUNCTION TO SEARCH A BOOK
-def search_book(dictonary):
+def search_book(library):
     searching_book = input("Enter name's book:\n")
     
     try:
-        copies = dictonary[searching_book]
-        print(f"Copies aviable of {searching_book} book: {copies}\n")  # OUTPUT OF COPIES AVIABLE OF SEARCHING BOOK
+        copies = library[searching_book]
+        print(f"Copies aviable of '{searching_book}': {copies}\n")  # OUTPUT OF COPIES AVIABLE OF SEARCHING BOOK
     except KeyError:
         print("Book not exist in the library.\n")
 
 #FUNCTION TO REMOVE A BOOK
-def remove_book(dictonary):
+def remove_book(library):
     searching_book_to_remove = input("Which book you want to remove? ")
 
     try:
-        del dictonary[searching_book_to_remove]
-        print(f"{searching_book_to_remove} book has been removed\n")  # OUTPUT OF REMOVED BOOK
+        del library[searching_book_to_remove]
+        print(f"'{searching_book_to_remove}' has been removed from library\n")  # OUTPUT OF REMOVED BOOK
     except KeyError:
         print("Book not exist in the library.\n")
 
@@ -72,17 +74,18 @@ def main():
     while choice < 1 or choice != 5:
         menu()
         choice = int(input("Enter a number between 1 to 5 to choise: "))
+        print()
         if choice < 1 or choice > 5:
             print("Error! Enter a valid number (1 to 5)\n")
     
         if choice == 1:
-            add_book(books)
+            add_book(library)
         elif choice == 2:
-            view_books(books)
+            view_books(library)
         elif choice == 3:
-            search_book(books)
+            search_book(library)
         elif choice == 4:
-            remove_book(books)
+            remove_book(library)
     print("Exit from program...")
      
 main()
